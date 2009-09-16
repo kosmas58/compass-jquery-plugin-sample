fdf10cf158d8bd7881d243c8d9ceda477c7ae811
kosmas58 <kosmas.schuetz@gmx.com>
2009-09-16 21:41:02 +0200

    W.i.P.
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- config/environment.rb
+++ config/environment.rb
@@ -40,6 +40,6 @@ Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]  
   config.i18n.default_locale = :en
 end
