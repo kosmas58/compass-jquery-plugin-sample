5a31c1962c8fafe97bacd2ebce220d7c55bce8b9
kosmas58 <kosmas.schuetz@gmx.com>
2009-12-05 18:44:28 +0100

    Updated to compass-jquery-plugin 0.2.4.5
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -20,9 +20,9 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   #config.gem 'json', :version => '>= 1.1.9'
-  config.gem 'haml', :version => '>= 2.2.13'
+  config.gem 'haml', :version => '>= 2.2.15'
   config.gem 'compass', :version => '>= 0.8.17', :source => "http://gemcutter.org"
-  config.gem 'compass-jquery-plugin', :version => '>= 0.2.4.4', :lib => 'jquery', :source => "http://gemcutter.org"
+  config.gem 'compass-jquery-plugin', :version => '>= 0.2.4.5', :lib => 'jquery', :source => "http://gemcutter.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
