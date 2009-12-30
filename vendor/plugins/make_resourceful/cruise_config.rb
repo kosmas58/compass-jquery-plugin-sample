be8691a8ff3007d3ca2f5fc840c2071e5d3b1c42
kosmas58 <kosmas.schuetz@gmx.com>
2009-12-30 15:46:12 +0100

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- vendor/plugins/make_resourceful/cruise_config.rb
+++ vendor/plugins/make_resourceful/cruise_config.rb
@@ -4,5 +4,5 @@ require 'fileutils'
 Project.configure do |project|
   project.rake_task = 'cruise:build'
   project.email_notifier.emails = ["admin@dconr.org" , "kosmas.schuetz@dconr.org"]
-  project.email_notifier.from = "CruiseControl.rb@dconr.org"
+  project.email_notifier.from = "admin@dconr.org"
 end
