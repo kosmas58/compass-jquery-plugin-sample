5af7f4af16e94148c6ff34791e34e31a804053aa
kosmas58 <kosmas.schuetz@gmx.com>
2010-01-12 21:31:25 +0100

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   #config.gem 'json', :version => '>= 1.1.9'
-  config.gem 'haml', :version => '>= 2.2.16'
+  config.gem 'haml', :version => '>= 2.2.17'
   config.gem 'compass', :version => '>= 0.8.17', :source => "http://gemcutter.org"
   config.gem 'compass-jquery-plugin', :version => '>= 0.2.4.8', :lib => 'jquery', :source => "http://gemcutter.org"
 
