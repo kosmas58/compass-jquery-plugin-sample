3f7c805c6f20a16a651e13078a96a134b88b27c9
kosmas58 <kosmas.schuetz@gmx.com>
2010-01-16 23:55:03 +0100

    Update to jQuery 1.4
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- config/environment.rb
+++ config/environment.rb
@@ -21,8 +21,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   #config.gem 'json', :version => '>= 1.1.9'
   config.gem 'haml', :version => '>= 2.2.17'
-  config.gem 'compass', :version => '>= 0.8.17', :source => "http://gemcutter.org"
-  config.gem 'compass-jquery-plugin', :version => '>= 0.2.4.99', :lib => 'jquery', :source => "http://gemcutter.org"
+  config.gem 'compass', :version => '>= 0.8.17'
+  config.gem 'compass-jquery-plugin', :version => '>= 0.2.4.100', :lib => 'jquery'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
