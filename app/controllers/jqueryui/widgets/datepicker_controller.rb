4794fabacd6f7e7f245868ba2ca76075afdbeab5
kosmas58 <kosmas.schuetz@gmx.com>
2010-02-20 17:59:04 +0100

    Localization
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/jqueryui/widgets/datepicker_controller.rb
+++ app/controllers/jqueryui/widgets/datepicker_controller.rb
@@ -1,5 +1,9 @@
 class Jqueryui::Widgets::DatepickerController < ApplicationController
-  layout 'blueprint'
-  
+  layout 'blueprint', :except => [:localization] 
   protect_from_forgery
+  
+  def localization
+    render :layout => "localization" 
+  end
+  
 end
