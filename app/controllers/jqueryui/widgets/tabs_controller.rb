6141f0154f8eaf6c97c087ae36f2e0e48629bb8d
kosmas58 <kosmas.schuetz@gmx.com>
2010-02-20 19:41:37 +0100

    Tabs
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/jqueryui/widgets/tabs_controller.rb
+++ app/controllers/jqueryui/widgets/tabs_controller.rb
@@ -2,4 +2,12 @@ class Jqueryui::Widgets::TabsController < ApplicationController
   layout 'blueprint'
   
   protect_from_forgery
+  
+  def content1
+    render :partial => "content1", :layout => false
+  end
+  
+  def content2
+    render :partial => "content2", :layout => false
+  end
 end
