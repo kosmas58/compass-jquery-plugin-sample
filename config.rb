a2b3da60dd12eff85c2473894a72b29dcea8047c
kosmas58 <kosmas.schuetz@gmx.com>
2010-02-22 20:28:01 +0100

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

---
+++ config.rb
@@ -0,0 +1,16 @@
+require 'jquery'
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/stylesheets/compiled"
+sass_dir = "app/stylesheets"
+images_dir = "public/images"
+javascripts_dir = "public/javascripts"
+output_style = :compact
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+http_images_path = "/images"
+http_stylesheets_path = "/stylesheets"
+http_javascripts_path = "/javascripts"
