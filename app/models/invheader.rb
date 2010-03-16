3ab64b50b1b09966ca182fc605bc90c92d41de60
kosmas58 <kosmas.schuetz@gmx.com>
2010-03-16 19:00:26 +0100

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/models/invheader.rb
+++ app/models/invheader.rb
@@ -25,5 +25,31 @@ class Invheader < ActiveRecord::Base
     :refresh_button => true,
     :pager          => true
     
+  gridify :demo02,
+    :title          => I18n.t('txt.jqgrid.demo.xml_data'),
+    :url            => "/jqgrid/demo",
+    :data_type      => :json,
+#    :colNames       => [
+#                        'Inv No',
+#                        I18n.t('activerecord.attributes.invheader.invdate'),
+#                        I18n.t('activerecord.attributes.invheader.client_id'),
+#                        I18n.t('activerecord.attributes.invheader.amount'),
+#                        I18n.t('activerecord.attributes.invheader.tax'),
+#                        I18n.t('activerecord.attributes.invheader.total'),
+#                        I18n.t('activerecord.attributes.invheader.notes')
+#                       ],
+#    :colModel       => [
+#                        { :name => 'id', :index => 'id', :width => 75 },
+#                        { :name => 'invdate', :index => 'invdate', :width => 90 },
+#                        { :name => 'name', :index => 'name', :width => 100 },
+#                        { :name => 'amount', :index => 'amount', :width => 80, :align => "right" },
+#                        { :name => 'tax', :index => 'tax', :width => 80, :align => "right" },    
+#                        { :name => 'total', :index => 'total', :width => 80, :align => "right" },   
+#                        { :name => 'note', :index => 'note', :width => 150, :sortable => false }   
+#                       ],  
+    :search_button  => true,  
+    :refresh_button => true,
+    :pager          => true
+    
 end
 
