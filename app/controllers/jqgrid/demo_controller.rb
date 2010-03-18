49154910f9dc5918db6aeafa31a90b9fb383ee74
kosmas58 <kosmas.schuetz@gmx.com>
2010-03-18 20:56:30 +0100

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/jqgrid/demo_controller.rb
+++ app/controllers/jqgrid/demo_controller.rb
@@ -9,7 +9,11 @@ class Jqgrid::DemoController < ApplicationController
     mylist = "demo#{@demo}".to_sym
     if request.xhr?
       records = Object.const_get(model).find_for_grid(mylist, params)
-      render :xml => Object.const_get(model).grid(mylist).encode_records(records)
+      if @demo == "01"
+        render :xml => Object.const_get(model).grid(mylist).encode_records(records)
+      else
+        render :json => Object.const_get(model).grid(mylist).encode_records(records)
+      end
     else
       @grid = Object.const_get(model).grid(mylist)
     end    
