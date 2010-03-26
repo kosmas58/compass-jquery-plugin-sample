71c126f9c3a435c4a36f8dd432385371e06add3a
kosmas58 <kosmas.schuetz@gmx.com>
2010-03-26 17:02:45 +0100

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   config.gem 'haml', :version => '>= 2.2.21'
-  config.gem 'compass', :version => '>= 0.8.17'
+  config.gem 'compass', :version => '= 0.8.17'
   config.gem 'compass-jquery-plugin', :version => '= 0.3.0.pre7', :lib => 'jquery'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
