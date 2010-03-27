5941eb33ce737f960ec8b5778779ebd64cdb8584
kosmas58 <kosmas.schuetz@gmx.com>
2010-03-27 07:10:30 +0100

    Cleanup
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/dynatree_controller.rb
+++ app/controllers/dynatree_controller.rb
@@ -11,22 +11,23 @@ class DynatreeController < ApplicationController
   
   def data1
     json1 = 
-    '[ { title: "Item 1" },
-       { title: "Folder 2", isFolder: true, key: "folder2",
-         children: [
-           { title: "Sub-item 2.1" },
-           { title: "Sub-item 2.2" }
-         ]
-       },
-       { title: "Folder 3", isFolder: true, key: "folder3",
-         children: [
-           { title: "Sub-item 3.1" ,
-           { title: "Sub-item 3.2 "}
-         ]
-       },
-       { title: "Lazy Folder 4", isFolder: true, isLazy: true, key: "folder4" },
-       { title: "Item 5" }
-     ]'
+    '[
+      {"title": "Item 1"},
+      {"title": "Folder 2", "isFolder": true, "key": "folder2",
+        "children": [
+          {"title": "Sub-item 2.1"},
+          {"title": "Sub-item 2.2"}
+        ]
+      },
+      {"title": "Folder 3", "isFolder": true, "key": "folder3",
+        "children": [
+          {"title": "Sub-item 3.1"},
+          {"title": "Sub-item 3.2"}
+        ]
+      },
+      {"title": "Lazy Folder 4", "isFolder": true, "isLazy": true, "key": "folder4"},
+      {"title": "Item 5"}
+    ]'
     
     respond_to do |format|
       # Fields order is important in the to_jqgrid_json method (in this case : [:id,:name])
@@ -36,14 +37,16 @@ class DynatreeController < ApplicationController
   end
   
   def data2
-    json2 = '[ {title: "SubItem 1", isLazy: true },  {title: "SubFolder 2", isFolder: true, isLazy: true } ]'
+    json2 = 
+    '[
+      {title: "SubItem 1", isLazy: true },
+      {title: "SubFolder 2", isFolder: true, isLazy: true }
+    ]'
     
     respond_to do |format|
       # Fields order is important in the to_jqgrid_json method (in this case : [:id,:name])
       # It must be the same as display order in your datagrid
       format.json { render :json => json2, :layout => false  }
-    end
-    
-  end
-  
+    end    
+  end  
 end
