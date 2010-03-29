53ea02823d397b040c1d186b1aa7b63230c347d2
kosmas58 <kosmas.schuetz@gmx.com>
2010-03-29 13:53:42 +0200

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/models/invheader.rb
+++ app/models/invheader.rb
@@ -28,6 +28,12 @@ class Invheader < ActiveRecord::Base
                          :ship_via => { :name => 'ship_via', :width =>  40 }
                        },
     :include        => [:client],
+    :sort_by        => :id,
+    :sort_order     => :desc,
+    :rows_per_page  => 10,     
+    :paging_choices => [10,20,30],  
+    #:autowidth:     => true, 
+    #:viewrecords    => true,    
     :search_button  => true,  
     :refresh_button => true,
     :pager          => true
@@ -36,6 +42,29 @@ class Invheader < ActiveRecord::Base
     :title          => I18n.t('txt.jqgrid.demo.json_data'),
     :url            => "/jqgrid/demo",
     :data_type      => :json,
+    :colNames       => [
+                        I18n.t('activerecord.attributes.invheader.id'),
+                        I18n.t('activerecord.attributes.invheader.invdate'),
+                        I18n.t('activerecord.attributes.invheader.client'),
+                        I18n.t('activerecord.attributes.invheader.amount'),
+                        I18n.t('activerecord.attributes.invheader.tax'),
+                        I18n.t('activerecord.attributes.invheader.total'),
+                        I18n.t('activerecord.attributes.invheader.note'),
+                        I18n.t('activerecord.attributes.invheader.closed'),
+                        I18n.t('activerecord.attributes.invheader.ship_via')
+                       ],
+    :colModel       => {
+                         :id       => { :name => 'id',       :width =>  50, :align => "right"  },
+                         :invdate  => { :name => 'invdate',  :width =>  90 },
+                         :name     => { :name => 'name',     :width => 100 },
+                         :amount   => { :name => 'amount',   :width =>  80, :align => "right" },
+                         :tax      => { :name => 'tax',      :width =>  80, :align => "right" },    
+                         :total    => { :name => 'total',    :width =>  80, :align => "right" },   
+                         :note     => { :name => 'note',     :width => 150, :sortable => false },
+                         :closed   => { :name => 'closed',   :width =>  40 },
+                         :ship_via => { :name => 'ship_via', :width =>  40 }
+                       },
+    :include        => [:client],
     :search_button  => true,  
     :refresh_button => true,
     :pager          => true
