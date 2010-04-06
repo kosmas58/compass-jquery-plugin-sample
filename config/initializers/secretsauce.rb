892bbd6e3884585a79d4987cae93f959683a83a2
kosmas58 <kosmas.schuetz@gmx.com>
2010-04-06 23:37:27 +0200

    Minor fix
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- config/initializers/secretsauce.rb
+++
@@ -1,8 +0,0 @@
-require 'jquery/secretsauce'
-
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :secretsauce => ["secretsauce.min"]
-
-#ActionView::Base.send :include, SecretSauce::Helpers::UiThemeHelper
-#ActionView::Base.send :include, SecretSauce::Helpers::UiGridHelper
-#ActionView::Base.send :include, SecretSauce::Helpers::UiFormHelper
-#ActionView::Base.send :include, SecretSauce::Helpers::UiDialogHelper
