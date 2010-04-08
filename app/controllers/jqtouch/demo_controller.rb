b6621646a07961f5b252a3de5fb7fba42d6f4084
kosmas58 <kosmas.schuetz@gmx.com>
2010-04-08 23:49:22 +0200

    Working jQTouch
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/jqtouch/demo_controller.rb
+++ app/controllers/jqtouch/demo_controller.rb
@@ -1,3 +1,15 @@
 class Jqtouch::DemoController < ApplicationController
   layout 'jqt_jqt'
+  
+  def ajax_post
+    @theaters = ["Los Gatos Cinema","Cinelux Plaza Theatre","Camera 7"];
+    @movies   = ["Transformers","Knocked Up","Live Free Die Hard"];
+    @title    = "-";
+#    if (params[:title]]) 
+#      $title = "Zip " . $_POST['zip'];
+#    else 
+#      $title = $_POST['movie'];
+#    end
+  end
+  
 end
