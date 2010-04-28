120cadad5a721c9621a4d318b3f7b1fba69a249f
kosmas58 <kosmas.schuetz@gmx.com>
2010-04-28 12:49:08 +0200

    Working on iCal demo
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/jqtouch/ical_controller.rb
+++ app/controllers/jqtouch/ical_controller.rb
@@ -2,4 +2,13 @@ class Jqtouch::IcalController < ApplicationController
   layout 'jqt_apple'
   
   protect_from_forgery
+  
+  def month
+    render :partial => "month", :layout => false
+  end
+  
+  def events
+    render :partial => "events", :layout => false
+  end
+  
 end
