9b0a56087a8b060b18eb071a56bbe94319ebc458
kosmas58 <kosmas.schuetz@gmx.com>
2010-05-02 00:05:42 +0200

    Working on jQTouch
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/views/jqtouch/demo2/get_gallery_array.php
+++
@@ -1,9 +0,0 @@
-<?php session_start() ?>
-<?php
-	$return_arr = array(); 
-
-	$return_arr = $_SESSION['galleryId']);
-
-
-	echo json_encode($return_arr);
-?>
--- app/views/jqtouch/demo2/show_image.php
+++
@@ -1,8 +0,0 @@
-<?php session_start() ?>
-<div id="show_image">
-<?php 
-    if (isset($_REQUEST['_SESSION'])) die("Get lost Dweeb!");
-	$fName = $_GET['fName'];
-	echo '<a class="slide-right" href="#"><img id="show_pic_id" src="'.$fName.'" BORDER="0"/></a>';
-?>
-</div>
