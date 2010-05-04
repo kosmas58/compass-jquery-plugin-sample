4900ec7536787e3900aec73da95c7c4ee0581b67
kosmas58 <kosmas.schuetz@gmx.com>
2010-05-04 08:17:01 +0200

    Updated to jQueryUI 1.8.1
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,7 @@ Rails::Initializer.run do |config|
 
   config.gem 'haml', :version => '= 3.0.0.rc.3'
   config.gem 'compass', :version => '= 0.10.0.rc5'
-  config.gem 'compass-jquery-plugin', :version => '= 0.3.0.beta.3', :lib => 'jquery'
+  config.gem 'compass-jquery-plugin', :version => '= 0.3.0.beta.4', :lib => 'jquery'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
