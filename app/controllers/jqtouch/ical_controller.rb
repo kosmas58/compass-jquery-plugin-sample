acbb37060f32c00aedd878fa6205fa9b9d011fcc
kosmas58 <kosmas.schuetz@gmx.com>
2010-05-05 23:21:56 +0200

    Working jQTouch iCal
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/jqtouch/ical_controller.rb
+++ app/controllers/jqtouch/ical_controller.rb
@@ -9,16 +9,38 @@ class Jqtouch::IcalController < ApplicationController
     
     @monthName = I18n.t('date.month_names')[@month]
     
-    @firstDay     = Date.new(@year, @month, 1)
-    @firstDayWeek = @firstDay.wday    
+    firstDay     = Date.new(@year, @month, 1)
+    firstDayWDay = firstDay.wday    
     
-    @lastDay     = (@firstDay>>1)-1 
-    @lastDayWeek = @lastDay.wday
+    lastDay     = (firstDay>>1)-1 
+    lastDayDay  = lastDay.strftime('%d').to_i
+    lastDayWDay = lastDay.wday
     
-    
-    #@lastMonday  = date('d-m-Y', strtotime('last Monday', strtotime($firstDay)));
-  
+    @cal = []
 
+    # Fill array with previous month days
+    if firstDayWDay != 1 then # first day of month not monday
+      preDays = firstDayWDay - 1
+      day = firstDay - preDays 
+      preDays.times do
+        @cal <<= { :day => day.strftime('%d').to_i, :type => 'prevmonth' } 
+        day += 1
+      end      
+    end
+    
+    # Fill array with month days    
+    (1..lastDayDay).each do |i| 
+      @cal <<= { :day => i, :type => 'normal' } 
+    end 
+
+    # Fill array with next month days
+    if lastDayWDay != 0 then # last day of month not sunday
+      day = lastDay + 1
+      (7-lastDayWDay).times do
+        @cal <<= { :day => day.strftime('%d').to_i, :type => 'nextmonth' } 
+        day += 1
+      end      
+    end
     
     render :partial => "month", :layout => false
   end
