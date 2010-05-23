041ba25ef0dbedffff21c65bcd5ac89398839a71
kosmas58 <kosmas.schuetz@gmx.com>
2010-05-23 09:32:03 +0200

    Working on calendar
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/jqical/anytime_controller.rb
+++ app/controllers/jqical/anytime_controller.rb
@@ -2,4 +2,8 @@ class Jqical::AnytimeController < ApplicationController
   layout 'jqical'
   
   protect_from_forgery
+  
+  def ajaxdemo
+    render(:text => params['AjaxDemo'].gsub(/\d+,/,"1"))
+  end
 end
