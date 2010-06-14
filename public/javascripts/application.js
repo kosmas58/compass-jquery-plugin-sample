95d6c3a7158060b9e7dbf43cbe1c0195ddaa8f5b
kosmas58 <kosmas.schuetz@gmx.com>
2010-06-14 20:38:28 +0200

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,3 @@
-jQuery.ajaxSetup({
-	'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-})
-
-jQuery.fn.submitWithAjax = function() {
-	this.submit(function() {
-    $.post($(this).attr("action"), $(this).serialize(), null, "script");
-    return false;
-  })
-}
-	
 jQuery(document).ready(function() {	
   // From http://dev.jqueryui.com/ticket/3613
 	// Cookie persistence missing
