f37f7bb53d6107d351cc260d322f0e28b3c6c209
kosmas58 <kosmas.schuetz@gmx.com>
2010-06-17 16:21:00 +0200

    Updated to jqGrid 3.7
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/models/item.rb
+++ app/models/item.rb
@@ -27,5 +27,35 @@ class Item < ActiveRecord::Base
     :sort_order          => :desc,
     :search_button       => true,  
     :refresh_button      => true,
-    :pager               => true       
+    :pager               => true
+  
+  gridify :demo1502,
+    :title               => I18n.t('txt.jqgrid.demo.37server'),
+    :url                 => "/jqgrid/demo",
+    :data_type           => :json,  
+    :colNames            => [
+                              'Index',
+                              'Name', 
+                              'Code'
+                            ],
+    :colModel            => {
+                              :id       => { :name  => 'id',
+                                             :width =>  65, 
+                                             :align => :right },
+                              :name     => { :name  => 'name',
+                                             :width => 150 },
+                              :cd       => { :name  => 'cd',
+                                             :width =>  100 }
+                            },                            
+    :row_numbers         => 40,
+    :width               => 700,
+    :height              => :auto,
+    :rows_per_page       => 100,
+    :paging_choices      => [10,20,30],
+    :sort_by             => :id,
+    :sort_order          => :desc,
+    :search_button       => true,  
+    :refresh_button      => true,
+    :pager               => true
+  
 end
