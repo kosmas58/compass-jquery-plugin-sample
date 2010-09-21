689ae8c706f888faaccab7032a6f656d8cb9f0c8
kosmas58 <kosmas.schuetz@gmx.com>
2010-09-21 19:47:14 +0200

    Working om languages
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,20 +12,4 @@ module ApplicationHelper
   def body_id
     "#{@controller.controller_name.dasherize}-#{@controller.action_name.dasherize}"
   end
-   
-  def options_for_select_with_class(container, selected = nil)
-    return container if String === container
-
-    container = container.to_a if Hash === container
-    selected, disabled = extract_selected_and_disabled(selected)
-
-    options_for_select = container.inject([]) do |options, element|
-      text, value = option_text_and_value(element)
-      selected_attribute = ' selected="selected"' if option_value_selected?(value, selected)
-      disabled_attribute = ' disabled="disabled"' if disabled && option_value_selected?(value, disabled)
-      options << %(<option class="#{html_escape(value.to_s)}" value="#{html_escape(value.to_s)}"#{selected_attribute}#{disabled_attribute}>#{html_escape(text.to_s)}</option>)
-    end
-
-    options_for_select.join("\n").html_safe
-  end  
 end
