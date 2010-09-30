efc50ba336b9753d99a3b366f9a4afac0238a6eb
kosmas58 <kosmas.schuetz@gmx.com>
2010-09-30 21:53:18 +0200

    Updated haml
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'haml', :version => '= 3.0.19'
+  config.gem 'haml', :version => '= 3.0.21'
   config.gem 'compass', :version => '= 0.10.5'
   config.gem 'compass-jquery-plugin', :version => '= 0.3.0.beta.22', :lib => 'jquery'
   config.gem 'ri_cal', :version => '= 0.8.7'
