cb58fb76bb64cbe13eb797cacbb7cb19719686b7
kosmas58 <kosmas.schuetz@gmx.com>
2011-01-16 14:31:54 +0100

    W.I.P.
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- db/migrate/20110112223000_create_list_items.rb
+++ db/migrate/20110112223000_create_list_items.rb
@@ -2,6 +2,7 @@ class CreateListItems < ActiveRecord::Migration
   def self.up
     create_table :list_items do |t|
       t.string :name
+      t.column :checked, :boolean, :default => false, :null => false
       t.timestamps
     end
   end
