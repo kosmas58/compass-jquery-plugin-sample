e4eb916dcc219f4d5889f512466e53e9ae331db0
kosmas58 <kosmas.schuetz@gmx.com>
2011-01-16 19:24:46 +0100

    Updated gems
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/manifest_controller.rb
+++ app/controllers/manifest_controller.rb
@@ -4,4 +4,18 @@ class ManifestController < ApplicationController
     headers['Content-Type'] = 'text/cache-manifest'
     render :text => Manifesto.cache, :layout => false
   end
+ 
+  require 'digest/md5'
+  require 'find'
+  
+  def self.compute_file_contents_hash(path)
+    hash = ''
+    digest = Digest::MD5.new
+    File.open(path, 'r') do |file|
+      digest.update(file.read(8192)) until file.eof
+      hash += digest.hexdigest
+    end
+    hash
+  end
+  
 end
