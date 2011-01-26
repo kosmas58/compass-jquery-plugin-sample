a3501f98cdba106e491bb4d595dec6305c17cd5b
kosmas58 <kosmas.schuetz@gmx.com>
2011-01-26 18:22:12 +0100

    Added rake taks from i18n-js
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-require "i18n-js"
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
