438ee5d2cbf4357e927b0e1f8a5da25afe8d0d60
kosmas58 <kosmas.schuetz@gmx.com>
2011-03-06 20:59:46 +0100

    W.I.P. Updated jqGrid
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/mobile/tuts/news_controller.rb
+++ app/controllers/mobile/tuts/news_controller.rb
@@ -12,9 +12,9 @@ class Mobile::Tuts::NewsController < ApplicationController
     # YQL query (SELECT * from feed ... ) // Split for readability
     path = "http://query.yahooapis.com/v1/public/yql?q="
     path += URI.escape("SELECT * FROM feed WHERE url='#{@val}'", Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-    path += "&format=json"
+    path += "&format=xml"
 
     # Call YQL, and if the query didn't fail, cache the returned data
-    render :json => Net::HTTP.get_response(URI.parse(path)).body
+    render :xml => Net::HTTP.get_response(URI.parse(path)).body
   end
 end
