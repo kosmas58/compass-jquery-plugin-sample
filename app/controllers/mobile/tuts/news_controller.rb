b1a8b54cc8bb4bfa74dcea479b0a529f02cd0657
kosmas58 <kosmas.schuetz@gmx.com>
2011-03-06 19:24:13 +0100

    W.I.P.
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/mobile/tuts/news_controller.rb
+++ app/controllers/mobile/tuts/news_controller.rb
@@ -1,9 +1,20 @@
+require 'uri'
+require 'net/http'
+
 class Mobile::Tuts::NewsController < ApplicationController
   layout 'mobile'
 
   protect_from_forgery
 
   def bridge
+    @val =  params[:fwd]
 
+    # YQL query (SELECT * from feed ... ) // Split for readability
+    path = "http://query.yahooapis.com/v1/public/yql?q="
+    path += URI.escape("SELECT * FROM feed WHERE url='#{@val}'", Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    path += "&format=json"
+
+    # Call YQL, and if the query didn't fail, cache the returned data
+    render :json => Net::HTTP.get_response(URI.parse(path)).body
   end
 end
