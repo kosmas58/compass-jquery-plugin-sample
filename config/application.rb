d8b39fae42005661f2db2fd1deb43df5a59152ff
kosmas58 <kosmas.schuetz@gmx.com>
2011-03-20 14:59:46 +0100

    After rails:upgrade:configuration
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- config/application.rb
+++ config/application.rb
@@ -1,42 +1,38 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
-
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
-
 module Sample3
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    # Add additional load paths for your own custom dirs
+    # config.load_paths += %W( #{Rails.root.to_s}/extras )
+
+    # Specify gems that this application depends on and have them installed with rake gems:install
+    # config.gem "bj"
+    # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
+    # config.gem "sqlite3-ruby", :lib => "sqlite3"
+    # config.gem "aws-s3", :lib => "aws/s3"
 
     # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
+    # :all can be used as a placeholder for all plugins not explicitly named
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-    # Activate observers that should always be running.
+    # Skip frameworks you're not going to use. To use Rails without a database,
+    # you must remove the Active Record framework.
+    # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+
+    # Activate observers that should always be running
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # Run "rake -D time" for a list of tasks for finding time zone names.
+    config.time_zone = 'UTC'
+    #config.time_zone = 'Berlin'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :en
   end
 end
