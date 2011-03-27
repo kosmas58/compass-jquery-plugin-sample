fc0ac33501f93675a321c67afbf1e04d9d35571a
kosmas58 <kosmas.schuetz@gmx.com>
2011-03-27 22:04:03 +0200

    Adding jquery.rails.js
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,19 +1,3 @@
-jQuery.ajaxSetup({
-	'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-})
-
-jQuery.fn.submitWithAjax = function() {
-	this.submit(function() {
-    $.post($(this).attr("action"), $(this).serialize(), null, "script");
-    return false;
-  })
-}
-
-$(document).ajaxSend(function(e, xhr, options) {
-  var token = $("meta[name='csrf-token']").attr("content");
-  xhr.setRequestHeader("X-CSRF-Token", token);
-});
-
 $(function() {
   $(window.applicationCache).bind("error", function() {
     alert("There was an error when loading the cache manifest");
