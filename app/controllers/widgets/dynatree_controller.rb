887548940b164df1faa1528393ecc29bb475c1b8
kosmas58 <kosmas.schuetz@gmx.com>
2011-04-23 00:24:38 +0200

    Continuing refactoring
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/widgets/dynatree_controller.rb
+++ app/controllers/widgets/dynatree_controller.rb
@@ -1,10 +1,10 @@
 class Widgets::DynatreeController < ApplicationController
-  layout 'dynatree', :except => [:iframe_1]
+  layout 'application', :except => [:iframe_1]
   protect_from_forgery
 
   def iframe_1
     respond_to do |format|
-      format.html { render :layout => 'dynatree_iframe' }
+      format.html { render :layout => 'dynatree' }
     end
   end
 
