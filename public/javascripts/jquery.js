1fbd482207345e5a92561cd5e829eb01d924c328
kosmas58 <kosmas.schuetz@gmx.com>
2011-05-15 15:15:23 +0200

    Updated gem
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -11,7 +11,7 @@
  * Copyright 2011, The Dojo Foundation
  * Released under the MIT, BSD, and GPL Licenses.
  *
- * Date: Fri May 13 16:33:42 +0200 2011
+ * Date: Sun May 15 14:58:43 +0200 2011
  */
 (function( window, undefined ) {
 
