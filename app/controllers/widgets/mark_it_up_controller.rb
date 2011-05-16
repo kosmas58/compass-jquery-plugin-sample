f09ee25908d09b0873b2ac7481bc31f17cbec450
kosmas58 <kosmas.schuetz@gmx.com>
2011-05-16 20:46:08 +0200

    W.I.P.
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/widgets/mark_it_up_controller.rb
+++ app/controllers/widgets/mark_it_up_controller.rb
@@ -1,6 +1,35 @@
 class Widgets::MarkItUpController < ApplicationController
   protect_from_forgery
 
+  def examples
+    @example = params[:example] || "basic"
+  end
+
+  def markitup
+    example = params[:example] || "basic"
+    render :partial => "markitup", :locals => {:example => example}
+  end
+
+  def simple
+    example = params[:example] || "basic"
+    render :partial => "simple", :locals => {:example => example}
+  end
+
+  def aqua
+    example = params[:example] || "basic"
+    render :partial => "aqua", :locals => {:example => example}
+  end
+
+  def live
+    example = params[:example] || "basic"
+    render :partial => "live", :locals => {:example => example}
+  end
+
+  def jtag
+    example = params[:example] || "basic"
+    render :partial => "jtag", :locals => {:example => example}
+  end
+
   def preview
     render :partial =>  "shared/mark_it_up_preview", :layout => false
   end
