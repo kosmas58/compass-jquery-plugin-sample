47c8b786c09696bf1e9c3195a5e1f0aecf86b0e1
kosmas58 <kosmas.schuetz@gmx.com>
2011-05-16 19:02:53 +0200

    Got the first maktItUp! example running.
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- app/controllers/widgets/mark_it_up_controller.rb
+++ app/controllers/widgets/mark_it_up_controller.rb
@@ -1,4 +1,8 @@
 class Widgets::MarkItUpController < ApplicationController
   protect_from_forgery
+
+  def preview
+    render :partial =>  "shared/mark_it_up_preview", :layout => false
+  end
   
 end
